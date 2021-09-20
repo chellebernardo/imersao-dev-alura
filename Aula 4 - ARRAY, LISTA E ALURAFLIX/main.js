08b80051c25d3795c3c79efdb7927e8caa59fe72
Michelle Bernardo <michellebernardo13@hotmail.com>
2021-09-20 20:59:18 -0300

    Aula 4 (unfinished)

---
+++ Aula 4 - ARRAY, LISTA E ALURAFLIX/main.js
@@ -0,0 +1,12 @@
+// var filme1 = "Yesterday"
+// var filme2 = "A chegada"
+// var filme3 = "Escola de Rock"
+
+var listaFilmes = ["Yesterday","A chegada", "Escola de Rock"]
+
+listaFilmes.push("Reri Poti")
+
+document.write("<p>"+listaFilmes[0]+"</p>");
+document.write("<p>"+listaFilmes[1]+"</p>");
+document.write("<p>"+listaFilmes[2]+"</p>");
+document.write("<p>"+listaFilmes[3]+"</p>");
